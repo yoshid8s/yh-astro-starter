3d060db48cf049ac8e64020ae3670c42b9f22264
Yoshifumi Takeuchi <takeuchi.y@dentsu.co.jp>
2026-07-27 16:11:26 +0900

    feat: add reusable WordPress article foundation

---
+++ src/lib/text.ts
@@ -0,0 +1,12 @@
+export function stripHtml(html: string) {
+  return html
+    .replace(/<[^>]*>/g, '')
+    .replace(/&nbsp;/g, ' ')
+    .replace(/&amp;/g, '&')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/\s+/g, ' ')
+    .trim();
+}
